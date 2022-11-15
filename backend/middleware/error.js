dab2c6c5cc755ab0d2162f0c83d42c9fb9bb708c
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-11-15 09:55:02 +0530

    resolve all type of Error handler

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,9 +3,16 @@ const ErrorHandler = require("../utils/erroehandler");
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server Error";
+  
 
+   // handle wrong mongodb Id or URl error
+    if(err.name === "CastError"){
+      const message = `Resource not found. Invalid : ${err.path}`
+      err = new ErrorHandler(message,400)
+    }
+   //else
   res.status(err.statusCode).json({
     success: false,
-    message: err.stack || err.message,
+    message: err.message || err.stack,
   });
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./Config/database");
+
+
+
+//handles the uncaught exceptions rejection
+process.on("uncaughtException", (err)=>{
+  console.log(`Error ${err.message}`);
+  console.log("shutting down server due to uncaught exceptions rejection");
+  process.exit(1);
+})
+
+
 // configuration
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -10,12 +21,14 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`server running on port : http://localhost:${process.env.PORT}`);
 });
 
+// console.log(jkfhsd)
+
 // unhandled Promise rejection in server
 process.on("unhandledRejection", (err) => {
   console.log(`Error ${err.message}`);
   console.log("shutting down server due to Unhandle Promise Rejection");
 
   server.close(() => {
-    process.exit();
+    process.exit(1);
   });
 });
